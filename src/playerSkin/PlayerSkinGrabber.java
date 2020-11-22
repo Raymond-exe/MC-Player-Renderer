6e749c503ff09b8efdee92009023f0af6f799f9c
Raymond-exe <Raymond-exe@users.noreply.github.com>
2020-11-22 13:26:53 -0800

    Changed default return for getSkin() (Issue #9)
    getSkin() will no longer return null for players who don't have a skin assigned (if they do, we're in trouble). Instead, the method should return a default Steve/Alex skin.
    
    Addresses issue #9.

--- src/playerSkin/PlayerSkinGrabber.java
+++ src/playerSkin/PlayerSkinGrabber.java
@@ -9,28 +9,36 @@ package playerSkin;
 
 import java.awt.image.BufferedImage;
 import java.net.URL;
+//import java.util.UUID;
 
 import javax.imageio.ImageIO;
+
+import playerSkin.PlayerSkin.SkinConfig;
 
 public class PlayerSkinGrabber {
 
 	private static final String SKIN_URL_PREFIX = "https://crafatar.com/skins/";	//append a UUID to the end of this link to get the player's skin file.
 	private static final String CAPE_URL_PREFIX = "https://crafatar.com/capes/";	//append a UUID to the end of this link to get the player's cape file.
 	
+	private static final String DEFAULT_SKIN_STEVE = "https://cdn.discordapp.com/attachments/774125506210693121/780156711281098762/default_steve.png";
+	private static final String DEFAULT_SKIN_ALEX  = "https://cdn.discordapp.com/attachments/774125506210693121/780156699133870090/default_alex.png";
+	
+	
 	/**
 	 * Retrieves a player's skin given their UUID
 	 * @param uuid A player's 32-character unique identifier
 	 * @return the current skin of the player affiliated with the UUID,
-	 * 		   or null if the UUID is invalid.
+	 * 		   or the ALEX/STEVE default skin if the UUID is invalid.
 	 */
 	public static BufferedImage getSkin(String uuid) {
-		try {
-			URL url = new URL(SKIN_URL_PREFIX + uuid);	  //makes a new URL object
-			BufferedImage imgOut = ImageIO.read(url); //reads the image at the given URL
-			return imgOut;							  //returns the image
-		} catch (Exception e) {
-			return null;							  //if the URL did not exist or
-		}											  //give an image, return null
+		BufferedImage img = retrieveImage(SKIN_URL_PREFIX + uuid);
+		
+		if(img == null) {
+			//img = getDefaultSkin(getSkinConfig(uuid));
+			img = getDefaultSkin(SkinConfig.STEVE); //TODO finish the getSkinConfig() method then remove this line
+		}
+		
+		return img;
 	}
 	
 	/**
@@ -40,8 +48,42 @@ public class PlayerSkinGrabber {
 	 * 		   or null if the player doesn't have a cape OR the UUID is invalid.
 	 */
 	public static BufferedImage getCape(String uuid) {
+		return retrieveImage(CAPE_URL_PREFIX + uuid);
+	}
+	
+	/**
+	 * Retrieves either an ALEX or STEVE default skin.
+	 * @param skinType Specifies either the STEVE or ALEX skin.
+	 * @return A STEVE or ALEX default skin.
+	 */	
+	public static BufferedImage getDefaultSkin(SkinConfig skinType) {
+		switch (skinType) {
+			case STEVE:
+				return retrieveImage(DEFAULT_SKIN_STEVE);
+			case ALEX:
+				return retrieveImage(DEFAULT_SKIN_ALEX);
+			default:
+				return null;
+		}
+	}
+	
+	
+	private static SkinConfig getSkinConfig(String uuid) {
+	    //TODO this method
+		return null;
+		
+		/*
+		UUID uid = UUID.fromString(uuid);
+	    if ((uid.hashCode() & 1) != 0) {
+	      System.out.println(uid.toString() + " = Alex");
+	    } else {
+	      System.out.println(uid.toString() + " = Steve");
+	    } //*/
+	  }
+	
+	private static BufferedImage retrieveImage(String urlAddress) {
 		try {
-			URL url = new URL(CAPE_URL_PREFIX + uuid);	  //makes a new URL object
+			URL url = new URL(urlAddress);	  //makes a new URL object
 			BufferedImage imgOut = ImageIO.read(url); //reads the image at the given URL
 			return imgOut;							  //returns the image
 		} catch (Exception e) {
